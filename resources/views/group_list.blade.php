032cd2a761c96c6336a90917dcece937925a50d0
PenguinHerder <skutelaandrzej@yahoo.com>
2018-11-24 11:37:42 +1300

    somewhat better groups presentation

--- resources/views/group_list.blade.php
+++ resources/views/group_list.blade.php
@@ -2,49 +2,51 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-12">
-            <div class="card">
-                <div class="card-header">
-					Groups
-					@can('create', App\Models\Group::class)
-					<a class='float-right' href='{{ route('groups.create') }}'>+New</a>
-					@endcan
-				</div>
+	<div class="alert alert-primary">
+		Groups
+	</div>
 
-                <div class="card-body">
-                    <table class="table table-striped">
-						<thead>
-							<tr>
-								<td>Name</td>
-								<td></td>
-							</tr>
-						</thead>
-						<tbody>
-							@foreach($groups as $group)
-							<tr>
-								<td>{{ $group->name }}</td>
-								<td><a href="{{ route('groups.show', ['group' => $group->id]) }}">Details</a></td>
-							</tr>
-							@endforeach
-							
-							@if($all !== null)
-							<tr>
-								<td colspan='2'>Other groups you are NOT a member of</td>
-							</tr>
-							
-								@foreach($all as $group)
-								<tr>
-									<td>{{ $group->name }}</td>
-									<td><a href="{{ route('groups.show', ['group' => $group->id]) }}">Details</a></td>
-								</tr>
-								@endforeach
-							@endif
-						</tbody>
-					</table>
-                </div>
-            </div>
-        </div>
-    </div>
+	<div class="row">
+		@foreach($groups as $group)
+		<div class="col-md-4">
+			<div class="card">
+				<div class="card-header">
+					<a href="{{ route('groups.show', ['group' => $group->id]) }}">{{ $group->name }}</a>
+				</div>
+			</div>
+		</div>
+		@endforeach
+
+		@can('create', App\Models\Group::class)
+		<div class="col-md-4">
+			<div class="card bg-info">
+				<div class="card-header">
+					<a href="{{ route('groups.show', ['group' => $group->id]) }}">+New</a>
+				</div>
+			</div>
+		</div>
+		@endcan
+	</div>
 </div>
+
+@if($all !== null)
+	<div class='container'>
+		<div class='alert alert-secondary'>
+			Other groups you are NOT a member of
+		</div>
+
+		<div class="row">
+			@foreach($all as $group)
+			<div class="col-md-4">
+				<div class="card">
+					<div class="card-header">
+						<a href="{{ route('groups.show', ['group' => $group->id]) }}">{{ $group->name }}</a>
+					</div>
+				</div>
+			</div>
+			@endforeach
+		</div>
+	</div>
+@endif
+
 @endsection
